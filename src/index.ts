a5ee0b5ac2804ac9d14636aa9b8a2b1b4f7371d5
killian ryan <killian_ryan@hotmail.com>
2022-04-08 20:59:42 +0100

    cookie added

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,70 @@
 import "reflect-metadata";
 import { ApolloServer } from "apollo-server-express";
 import express from "express";
-import { buildSchema, format } from "type-graphql";
+import { buildSchema } from "type-graphql";
+import session from "express-session";
+import connectRedis from "connect-redis";
+import cors from "cors";
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "apollo-server-core";
 
 import { AppDataSource } from "./data-source";
 import { RegisterResolver } from "./modules/user/Register";
+import { redis } from "./redis";
+import { LoginResolver } from "./modules/user/Login";
 
+declare module "express-session" {
+  interface SessionData {
+    userId: any;
+  }
+}
 const main = async () => {
   await AppDataSource.initialize();
 
   const schema = await buildSchema({
-    resolvers: [RegisterResolver],
+    resolvers: [RegisterResolver, LoginResolver],
     emitSchemaFile: true,
   });
 
-  const apolloServer = new ApolloServer({ schema });
+  const apolloServer = new ApolloServer({
+    schema,
+    context: ({ req }: any) => ({ req }),
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground],
+  });
 
   await apolloServer.start();
 
   const app = express();
+
+  const RedisStore = connectRedis(session);
+
+  app.use(
+    cors({
+      credentials: true,
+      origin: "https://studio.apollographql.com",
+    })
+  );
+
+  // const corsOptions = {
+  //   origin: "https://studio.apollographql.com",
+  //   credentials: true,
+  // };
+
+  app.use(
+    session({
+      store: new RedisStore({
+        client: redis,
+      }),
+      name: "qid",
+      secret: "aslkdfjoiq12312",
+      resave: false,
+      saveUninitialized: false,
+      cookie: {
+        httpOnly: true,
+        secure: process.env.NODE_ENV === "production",
+        maxAge: 1000 * 60 * 60 * 24 * 7 * 365, // 7 years
+      },
+    })
+  );
 
   apolloServer.applyMiddleware({ app });
 
